3139d8b3d2d21774cda2e9084ca1378f56c3fa47
ksd <kimseong124@naver.com>
2024-08-20 20:35:32 +0900

    set background

--- code/level.py
+++ code/level.py
@@ -1,5 +1,5 @@
 # level setting
-import pygame
+import pygame, os
 from setting import *
 from tile import Tile
 from player import Player
@@ -19,14 +19,14 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_idx, row in enumerate(WORLD_MAP):
-            for col_idx, col in enumerate(row):
-                x = col_idx * TILESIZE
-                y = row_idx * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        # for row_idx, row in enumerate(WORLD_MAP):
+        #     for col_idx, col in enumerate(row):
+        #         x = col_idx * TILESIZE
+        #         y = row_idx * TILESIZE
+        #         if col == 'x':
+        #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
         
     def run(self):
         # update and draw the game
@@ -44,12 +44,23 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        base_path = os.path.dirname(__file__)
+        floor_image_path = os.path.join(base_path, '../graphics/tilemap/ground.png')
+        self.floor_surface = pygame.image.load(floor_image_path).convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
 
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
+        # draw floor
+        floor_offset_position = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_position)
+
+        # draw sprites
         # sorted함수를 사용한 이유 : y좌표를 기준으로 정렬하여 y좌표가 더 큰 것이 나중에 그려져야 함.
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_position = sprite.rect.topleft - self.offset
